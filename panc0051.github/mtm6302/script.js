cb509e3bcf92de7716dbe8a48980a0e22bb8272e
panc0051 <panc0051@algonquinlive.com>
2022-01-13 10:11:05 -0500

    first javascript

---
+++ panc0051.github/mtm6302/script.js
@@ -0,0 +1,12 @@
+// Writing name: Panc0051
+const firstName = 'Preet'
+const lastName = 'Panchal'
+
+// declaring age
+let age = 23
+
+//using boolean
+const inclass = true
+
+//template literal
+const message = `My name is ${firstName} ${lastName} and I am ${age} years old.`
